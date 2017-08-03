243643b02ca51b542bf615a33e2aa26d041c1b00
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-08-02 22:31:38 -0400

    css practice / foundation

--- js-fetch-challenge/scripts/fetchImage.js
+++ js-fetch-challenge/scripts/fetchImage.js
@@ -1,10 +1,10 @@
 ready().then(() => {
   fetch('/images/logo.png')
   .then(response => response.blob())
-  .then(blob => objectURL = URL.createObjectURL(blob))
-  .then(url => {
+  .then(blob => {
+    let objectURL = URL.createObjectURL(blob);
     let imageElement = document.createElement('img');
-    imageElement.src = url;
+    imageElement.src = objectURL;
     document.getElementById('main').appendChild(imageElement);
   });
 });
